659f34743b0438d732e231007899a0661cd993c3
SmellsNice <80067367+SmellsNice@users.noreply.github.com>
2021-09-01 16:59:43 +0300

    Add files via upload

--- config.h
+++ config.h
@@ -18,8 +18,8 @@ enum {
  * (first/last value is used as min/max zoom level)
  */
 static const float zoom_levels[] = {
-	 6.25, 12.5, 18.75,  25.0, 37.5,  50.0, 62.5, 75.0,
-	100.0, 125.0, 150.0, 175.0, 200.0, 300.0, 400.0, 600.0, 800.0
+         10.0, 	20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 
+	100.0, 110.0, 120.0, 130.0, 140.0, 150.0, 160.0, 170.0, 180.0, 190.0, 200.0, 225.0, 250.0, 275.0, 300.0, 325.0, 350.0, 375.0, 400.0, 450.0, 500.0, 550.0, 600.0, 650.0, 700.0, 750.0, 800.0, 850.0, 900.0, 950.0, 1000.0
 };
 
 /* default slideshow delay (in sec, overwritten via -S option): */
@@ -32,7 +32,7 @@ static const double GAMMA_MAX   = 10.0;
 static const int    GAMMA_RANGE = 32;
 
 /* command i_scroll pans image 1/PAN_FRACTION of screen width/height */
-static const int PAN_FRACTION = 5;
+static const int PAN_FRACTION = 25;
 
 /* if false, pixelate images at zoom level != 100%,
  * toggled with 'a' key binding
@@ -76,23 +76,29 @@ static const int THUMB_SIZE = 3;
 static const keymap_t keys[] = {
 	/* modifiers    key               function              argument */
 	{ 0,            XK_q,             g_quit,               None },
-	#if DMENU_LIKE_BEHAVIOUR_PATCH
+  	{ ControlMask,  XK_c,             g_quit,               None },
+        #if DMENU_LIKE_BEHAVIOUR_PATCH
 	{ 0,            XK_Return,        g_switch_mode,        0 },
 	{ ControlMask,  XK_Return,        g_switch_mode,        1 },
 	#else
 	{ 0,            XK_Return,        g_switch_mode,        None },
 	#endif // DMENU_LIKE_BEHAVIOUR_PATCH
-	{ ControlMask,  XK_f,             g_toggle_fullscreen,  None },
+	{ 0,            XK_f,             g_toggle_fullscreen,  None },
 	{ 0,            XK_b,             g_toggle_bar,         None },
 	{ ControlMask,  XK_x,             g_prefix_external,    None },
 	{ 0,            XK_g,             g_first,              None },
 	{ 0,            XK_G,             g_n_or_last,          None },
 	{ 0,            XK_r,             g_reload_image,       None },
 	{ 0,            XK_D,             g_remove_image,       None },
-	{ ControlMask,  XK_h,             i_scroll_to_edge,     DIR_LEFT },
-	{ ControlMask,  XK_j,             i_scroll_to_edge,     DIR_DOWN },
-	{ ControlMask,  XK_k,             i_scroll_to_edge,     DIR_UP },
-	{ ControlMask,  XK_l,             i_scroll_to_edge,     DIR_RIGHT },
+	{ ShiftMask,    XK_h,             i_scroll_to_edge,     DIR_LEFT },
+	{ ShiftMask,    XK_j,             i_scroll_to_edge,     DIR_DOWN },
+	{ ShiftMask,    XK_k,             i_scroll_to_edge,     DIR_UP },
+	{ ShiftMask,    XK_l,             i_scroll_to_edge,     DIR_RIGHT },
+	{ ShiftMask,    XK_Left,          i_scroll_to_edge,     DIR_LEFT },
+	{ ShiftMask,    XK_Down,          i_scroll_to_edge,     DIR_DOWN },
+	{ ShiftMask,    XK_Up,            i_scroll_to_edge,     DIR_UP },
+	{ ShiftMask,    XK_Right,         i_scroll_to_edge,     DIR_RIGHT },
+
 	{ 0,            XK_m,             g_toggle_image_mark,  None },
 	{ 0,            XK_M,             g_mark_range,         None },
 	{ ControlMask,  XK_m,             g_reverse_marks,      None },
@@ -106,21 +112,24 @@ static const keymap_t keys[] = {
 	{ 0,            XK_braceleft,     g_change_gamma,        0 },
 	{ 0,            XK_braceright,    g_change_gamma,        0 },
 
-	{ 0,            XK_h,             t_move_sel,           DIR_LEFT },
-	{ 0,            XK_j,             t_move_sel,           DIR_DOWN },
-	{ 0,            XK_k,             t_move_sel,           DIR_UP },
-	{ 0,            XK_l,             t_move_sel,           DIR_RIGHT },
-	{ 0,            XK_Left,          t_move_sel,           DIR_LEFT },
-	{ 0,            XK_Down,          t_move_sel,           DIR_DOWN },
-	{ 0,            XK_Right,         t_move_sel,           DIR_RIGHT },
-	{ 0,            XK_Up,            t_move_sel,           DIR_UP },
+	{ ControlMask,  XK_h,             t_move_sel,           DIR_LEFT },
+	{ ControlMask,  XK_j,             t_move_sel,           DIR_DOWN },
+	{ ControlMask,  XK_k,             t_move_sel,           DIR_UP },
+	{ ControlMask,  XK_l,             t_move_sel,           DIR_RIGHT },
+	{ ControlMask,  XK_Left,          t_move_sel,           DIR_LEFT },
+	{ ControlMask,  XK_Down,          t_move_sel,           DIR_DOWN },
+	{ ControlMask,  XK_Right,         t_move_sel,           DIR_RIGHT },
+	{ ControlMask,  XK_Up,            t_move_sel,           DIR_UP },
 	{ 0,            XK_R,             t_reload_all,         None },
 
 	{ 0,            XK_n,             i_navigate,           +1 },
 	{ 0,            XK_space,         i_navigate,           +1 },
-	{ 0,            XK_p,             i_navigate,           -1 },
+	{ 0,            XK_Right,         i_navigate,           +1 },
+        { 0,            XK_p,             i_navigate,           -1 },
 	{ 0,            XK_BackSpace,     i_navigate,           -1 },
-	{ ControlMask,  XK_6,             i_alternate,          None },
+	{ ShiftMask,    XK_space,         i_navigate,           -1 },
+        { 0,            XK_Left,          i_navigate,           -1 },
+        { ControlMask,  XK_6,             i_alternate,          None },
 	{ ControlMask,  XK_space,         i_toggle_animation,   None },
 	#if NAVIGATE_IF_NOT_SCROLL_PATCH
 	{ 0,            XK_h,             i_scroll_or_navigate, DIR_LEFT },
@@ -132,20 +141,26 @@ static const keymap_t keys[] = {
 	{ 0,            XK_l,             i_scroll_or_navigate, DIR_RIGHT },
 	{ 0,            XK_Right,         i_scroll_or_navigate, DIR_RIGHT },
 	#else
-	{ 0,            XK_h,             i_scroll,             DIR_LEFT },
-	{ 0,            XK_Left,          i_scroll,             DIR_LEFT },
-	{ 0,            XK_j,             i_scroll,             DIR_DOWN },
-	{ 0,            XK_Down,          i_scroll,             DIR_DOWN },
-	{ 0,            XK_k,             i_scroll,             DIR_UP },
-	{ 0,            XK_Up,            i_scroll,             DIR_UP },
-	{ 0,            XK_l,             i_scroll,             DIR_RIGHT },
-	{ 0,            XK_Right,         i_scroll,             DIR_RIGHT },
+	{ ControlMask,  XK_h,             i_scroll,             DIR_LEFT },
+	{ ControlMask,  XK_Left,          i_scroll,             DIR_LEFT },
+	{ ControlMask,  XK_j,             i_scroll,             DIR_DOWN },
+	{ ControlMask,  XK_Down,          i_scroll,             DIR_DOWN },
+	{ ControlMask,  XK_k,             i_scroll,             DIR_UP },
+	{ ControlMask,  XK_Up,            i_scroll,             DIR_UP },
+	{ ControlMask,  XK_l,             i_scroll,             DIR_RIGHT },
+	{ ControlMask,  XK_Right,         i_scroll,             DIR_RIGHT },
 	#endif // NAVIGATE_IF_NOT_SCROLL_PATCH
 	{ 0,            XK_J,             g_zoom,               -1 },
-	{ 0,            XK_K,             g_zoom,               +1 },
-	{ 0,            XK_f,             i_set_zoom,           100 },
-	{ 0,            XK_w,             i_fit_to_win,         SCALE_DOWN },
-	{ 0,            XK_W,             i_fit_to_win,         SCALE_FIT },
+	{ 0,            XK_K,             g_zoom,               +1 },	
+	{ 0,            XK_Down,          g_zoom,               -1 },
+	{ 0,            XK_Up,            g_zoom,               +1 },
+	{ 0,            XK_KP_Subtract,   g_zoom,               -1 },
+	{ 0,            XK_KP_Add,        g_zoom,               +1 },
+        { 0,            XK_minus,         g_zoom,               -1 },
+	{ 0,            XK_plus,          g_zoom,               +1 },
+        { 0,            XK_equal,         i_set_zoom,           100 },
+	{ 0,            XK_W,             i_fit_to_win,         SCALE_DOWN },
+	{ 0,            XK_w,             i_fit_to_win,         SCALE_FIT },
 	#if SCALE_FILL_MODE_PATCH
 	{ 0,            XK_F,             i_fit_to_win,         SCALE_FILL },
 	#endif // SCALE_FILL_MODE_PATCH
